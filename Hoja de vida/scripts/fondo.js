4ee496b755b91c308265c7060e442430b68a29b6
Vane789 <78865050+Vane789@users.noreply.github.com>
2021-03-16 21:47:02 -0500

    Actualizado hoja de vida con un poco de js

---
+++ Hoja de vida/scripts/fondo.js
@@ -0,0 +1,13 @@
+/*Anlly Vanessa Cardona*/
+function Color(){
+	let colorl= document.getElementsByClassName("titulos");
+	
+	for(let i=0;i<colorl.length; i++){
+		
+	 colorl[i].style.backgroundColor= "#000000";
+	}
+	
+	let color2= document.getElementById("tabla");
+	color2.style.color="red";
+
+}
